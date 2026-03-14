8db2419857383489ad9d93dbf6cdf66fcc9e1272
Sambal Shrestha <splashofwater900@gmail.com>
2026-03-14 19:15:11 +0000

    Inline Artifact Rendering Line Magic Prototype

--- src/metaflow_jupyter/magics.py
+++ src/metaflow_jupyter/magics.py
@@ -1,8 +1,9 @@
-import os
+import os, sys, json
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
+from IPython.display import display, HTML
 from metaflow import Runner
 from .registry import registry
-import sys
+from metaflow import Flow
 
 
 @magics_class
@@ -132,6 +133,119 @@ class MetaflowMagics(Magics):
             if not export and os.path.exists(filename):
                 os.remove(filename)
 
+    @line_magic
+    def mf_show(self, line):
+        """
+        %mf_show [Pathspec] [ArtifactName]
+        """
+
+        args = line.strip().split()
+
+        # Combine args into a single string for PathSpec parsing
+        combined_args = " ".join(args)
+
+        # If '.' is present in the combined args, it is either: step.artifact or flow step.artifact
+        # In such case we split step from artifact
+        if "." in combined_args:
+            path, artifact = combined_args.rsplit(".", 1)
+
+        # And if there is no '.' and was made from exactly two args, it is: flow/run/step artifact
+        elif len(args) == 2:
+            path, artifact = args
+        else:
+            return print("Usage: 'step.artifact' or 'flow step.artifact' or 'flow/run/step artifact or flow'")
+
+        # For the splitted step, we first combine it with the PathSpec
+        # Then we split the path into segments to check for flow, run_id and step
+        path_segments = [segment for segment in path.replace(" ", "/").split("/") if segment]
+        
+        # Set default values Flow and run_id whereas step value will always be given 
+        flow, run_id, step = "MyFlow", "latest", None
+
+        if len(path_segments) == 3:
+            # For: flow/run/step from flow/run/step artifact
+            flow, run_id, step = path_segments
+        elif len(path_segments) == 2:
+            # For flow/step from flow step.artifact
+            flow, step = path_segments
+        elif len(path_segments) == 1:
+            # For step from step.artifact
+            step = path_segments[0]
+
+        # Fetch and Render Artifact 
+        try:
+            
+            flow_obj = Flow(flow)
+            run_obj = flow_obj.latest_run if run_id == "latest" else flow_obj[run_id]
+            step_obj = run_obj[step]
+                
+            tasks = list(step_obj.tasks())
+            count = 0
+
+            # A step can have multiple tasks (foreach). So rendering artifact of each
+            for task in tasks:
+                try:
+                    # While unpickling a value, we need the corresponding library to reconstruct the object
+                    # For eg: Pandas->Dataframe, Matplotlib->Figure, Numpy->ndarray
+                    # So if any of the library corresponding to the artifact's type is missing, it will raise an error
+                    val = getattr(task.data, artifact)
+                    count += 1
+                except AttributeError:
+                    # If we dont find any artifact in the current task, we skip it
+                    continue
+            
+                # Trace all the foreach parameters we have passed through to reach the current task 
+                foreach_stack = task.metadata_dict.get('foreach-stack', [])
+                foreach_label = ""
+                if foreach_stack:
+                    # Join all the foreach parameters into a comma seperate string 'param1 = value1, param2 = value2'
+                    foreach_label = ', '.join(foreach_stack)
+
+                # Extract flow_id and step_id from the pathspec (Muting run_id and task_id)
+                flow_id, _ , step_id, _ = task.pathspec.split('/')
+
+                # Display header for the artifact
+                header = (
+                    f"&nbsp; <b>Flow:</b> {flow_id} &nbsp;"
+                    f"|&nbsp; <b>Step:</b> {step_id} &nbsp;"
+                    f"|&nbsp; <b>Artifact:</b> {artifact} &nbsp;"
+                )
+                if foreach_label:
+                    header += f"|&nbsp; <b>Foreach Stack:</b> ( {foreach_label} ) &nbsp;"
+                display(HTML(f"<code>{header}</code>"))
+
+                # Display the artifact considering it's type
+                if "numpy" in str(type(val)):
+                    print(f"numpy.ndarray: shape={val.shape}, dtype={val.dtype}")
+                    display(val)
+                elif isinstance(val, (dict, list)):
+                    print(json.dumps(val, indent=2))
+                else:
+                    try:
+                        display(val)
+                    except Exception as e:
+                        print(f"Error: {e}")
+
+                # Close matplotlib figures after display to prevent double rendering
+                mpl = sys.modules.get("matplotlib")
+                if mpl and isinstance(val, mpl.figure.Figure):
+                    try:
+                        mpl.pyplot.close(val)
+                    except Exception as e:
+                        # The only error that can come from mpl.pyplot.close(val) is due to close() as mpl is not None 
+                        # This means the figure was already closed in the flow. So we can't again call close on it
+                        # This also signals that the figure we tried rendering before wasn't successfull (as it was already closed before rendering)
+                        print("Note: This figure was closed with .close() before being saved.\n"
+                                "      Remove .close() from your flow to display it here.\n"
+                                "      (Memory is freed automatically when the step completes, so .close() isn't needed)")
+
+            # If no artifact was found in any of the tasks, display that no such artifact was found in the given step
+            if count == 0:
+                print(f"Artifact '{artifact}' not found in step '{step_id}'")
+        except Exception as e:
+            # Printing the error message manually to avoid complex traceback and thus make the error message clear
+            # Also as this line magic neither return nor affect anything, we can resume other cell execution (so we don't raise any error)
+            print(f"Error: {e}")
 
 def register_magics(ipython):
     ipython.register_magics(MetaflowMagics)
